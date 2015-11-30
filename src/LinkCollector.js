49ff54c3bee97dd22d626519465f07d15f1db522
Dominik Staskiewicz <stadominik@gmail.com>
2015-11-30 19:04:22 +0100

    Tidy up

--- src/LinkCollector.js
+++ src/LinkCollector.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React          from 'react/addons';
-import ReactDOM from 'react-dom';
-import Login from 'components/Login';
+import ReactDOM       from 'react-dom';
+import Login          from 'components/Login';
 
 ReactDOM.render(<Login />, document.getElementById('main'));
